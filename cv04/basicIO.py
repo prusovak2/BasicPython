a812efb7492279a875e558de752fa777835cbe85
prusovak2 <prusova.k2@seznam.cz>
2020-03-19 15:12:56 +0100

    basic IO finished

--- cv04/basicIO.py
+++ cv04/basicIO.py
@@ -6,7 +6,10 @@ else:
     counter = 0
     with open(sys.argv[1]) as inputFile:
         for line in inputFile:
-            for word in line:
-                counter += 1
+            for word in line.split():
                 print(word)
+                counter+=1
+                print(counter)
+        print()
+        print(counter)
                 
